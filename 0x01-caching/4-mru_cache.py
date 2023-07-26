e8d487e32ef01d8deb42a9c07d66307ff8dd881b
Rotondwa Ishmael Ndou <111059267+ndouishmael@users.noreply.github.com>
2023-07-26 22:06:12 +0200

    Update 4-mru_cache.py

--- 0x01-caching/4-mru_cache.py
+++ 0x01-caching/4-mru_cache.py
@@ -1,39 +1,47 @@
 #!/usr/bin/python3
-""" MRUCache module"""
+""" MRU Caching """
 from base_caching import BaseCaching
 
 
 class MRUCache(BaseCaching):
-    """ MRUCache inherits from BaseCaching and is a caching system
-    """
-
+    """ Class that inherits from BaseCaching and is a caching system """
     def __init__(self):
-        """ Initialize MRUCache
-        """
         super().__init__()
-        self.order = []  # To track the order of item usage
+        self.head, self.tail = 'head', 'tail'
+        self.next, self.prev = {}, {}
+        self.handle(self.head, self.tail)
+
+    def handle(self, head, tail):
+        """ MRU algorithm, handle elements """
+        self.next[head], self.prev[tail] = tail, head
+
+    def _remove(self, key):
+        """ MRU algorithm, remove element """
+        self.handle(self.prev[key], self.next[key])
+        del self.prev[key], self.next[key], self.cache_data[key]
+
+    def _add(self, key, item):
+        """ MRU algorithm, add element """
+        if len(self.cache_data) > BaseCaching.MAX_ITEMS - 1:
+            print("DISCARD: {}".format(self.prev[self.tail]))
+            self._remove(self.prev[self.tail])
+        self.cache_data[key] = item
+        self.handle(self.prev[self.tail], key)
+        self.handle(key, self.tail)
 
     def put(self, key, item):
-        """ Add an item in the cache using MRU algorithm
-        """
-        if key is not None and item is not None:
+        """ Assign to the dictionary """
+        if key and item:
             if key in self.cache_data:
-                self.order.remove(key)  # Remove the key from the order list
-            elif len(self.cache_data) >= self.MAX_ITEMS:
-                # Get the most recently used key from the end of the order list
-                mru_key = self.order.pop()
-                print(f"DISCARD: {mru_key}")
-                del self.cache_data[mru_key]
-
-            self.cache_data[key] = item
-            self.order.append(key)  # Append the key to the end of the order list
+                self._remove(key)
+            self._add(key, item)
 
     def get(self, key):
-        """ Get an item by key from the cache
-        """
-        if key is not None and key in self.cache_data:
-            # Move the key to the end of the order list to indicate it was recently used
-            self.order.remove(key)
-            self.order.append(key)
-            return self.cache_data[key]
-        return None
+        """ Return the value linked """
+        if key is None or self.cache_data.get(key) is None:
+            return None
+        if key in self.cache_data:
+            value = self.cache_data[key]
+            self._remove(key)
+            self._add(key, value)
+            return value
