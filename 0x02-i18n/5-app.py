9c3fcd9a29a89ad9beb263ea79ff6475100426f4
Rotondwa Ishmael Ndou <111059267+ndouishmael@users.noreply.github.com>
2023-08-01 21:03:34 +0200

    Update 5-app.py

--- 0x02-i18n/5-app.py
+++ 0x02-i18n/5-app.py
@@ -1,23 +1,18 @@
 #!/usr/bin/env python3
-"""A Basic Flask app with internationalization support.
+"""0-app module
 """
-from flask_babel import Babel
-from typing import Union, Dict
 from flask import Flask, render_template, request, g
+from flask_babel import Babel
 
 
-class Config:
-    """Represents a Flask Babel configuration.
+class Config(object):
+    """Config object class
     """
     LANGUAGES = ["en", "fr"]
     BABEL_DEFAULT_LOCALE = "en"
     BABEL_DEFAULT_TIMEZONE = "UTC"
 
 
-app = Flask(__name__)
-app.config.from_object(Config)
-app.url_map.strict_slashes = False
-babel = Babel(app)
 users = {
     1: {"name": "Balou", "locale": "fr", "timezone": "Europe/Paris"},
     2: {"name": "Beyonce", "locale": "en", "timezone": "US/Central"},
@@ -25,40 +20,48 @@ users = {
     4: {"name": "Teletubby", "locale": None, "timezone": "Europe/London"},
 }
 
+app = Flask(__name__)
+app.config.from_object(Config)
+app.url_map.strict_slashes = False
+babel = Babel(app)
 
-def get_user() -> Union[Dict, None]:
-    """Retrieves a user based on a user id.
+
+@babel.localeselector
+def get_locale():
+    """get_locale function
+
+    Returns:
+        str - locale
     """
-    login_id = request.args.get('login_as')
-    if login_id:
-        return users.get(int(login_id))
+    if request.args.get("locale") in app.config['LANGUAGES']:
+        return request.args.get("locale")
+    return request.accept_languages.best_match(app.config["LANGUAGES"])
+
+
+def get_user():
+    """get_user function
+
+    Returns:
+        dict - user
+    """
+    if request.args.get("login_as"):
+        return users.get(int(request.args.get("login_as")))
     return None
 
 
 @app.before_request
-def before_request() -> None:
-    """Performs some routines before each request's resolution.
+def before_request():
+    """before_request function
     """
-    user = get_user()
-    g.user = user
+    g.user = get_user()
 
 
-@babel.localeselector
-def get_locale() -> str:
-    """Retrieves the locale for a web page.
+@app.route("/", methods=["GET"])
+def home():
+    """index function
     """
-    locale = request.args.get('locale', '')
-    if locale in app.config["LANGUAGES"]:
-        return locale
-    return request.accept_languages.best_match(app.config["LANGUAGES"])
+    return render_template("6-index.html")
 
 
-@app.route('/')
-def get_index() -> str:
-    """The home/index page.
-    """
-    return render_template('5-index.html')
-
-
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port="5000", debug=True)
