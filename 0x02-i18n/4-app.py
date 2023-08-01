45348c6646109618f4465cec80bf890fa9a023f3
Rotondwa Ishmael Ndou <111059267+ndouishmael@users.noreply.github.com>
2023-08-01 20:43:59 +0200

    Update 4-app.py

--- 0x02-i18n/4-app.py
+++ 0x02-i18n/4-app.py
@@ -1,41 +1,48 @@
 #!/usr/bin/env python3
-""" Basic Flask app, Basic Babel setup, Get locale from request,
-    Parametrize templates, Force locale with URL parameter """
-from flask import Flask, render_template, request
-from flask_babel import Babel, gettext
-
-app = Flask(__name__)
-babel = Babel(app)
-""" instantiate the Babel object """
+"""
+A Basic flask application
+"""
+from flask import Flask
+from flask import request
+from flask import render_template
+from flask_babel import Babel
 
 
 class Config(object):
-    """ config class """
+    """
+    Application configuration class
+    """
     LANGUAGES = ['en', 'fr']
     BABEL_DEFAULT_LOCALE = 'en'
     BABEL_DEFAULT_TIMEZONE = 'UTC'
 
 
+# Instantiate the application object
+app = Flask(__name__)
 app.config.from_object(Config)
-""" Use that class as config for Flask app """
 
-
-@app.route('/')
-def root():
-    """ basic Flask app """
-    return render_template("4-index.html")
+# Wrap the application with Babel
+babel = Babel(app)
 
 
 @babel.localeselector
-def get_locale():
-    """ to determine the best match with our supported languages """
-    localLang = request.args.get('locale')
-    supportLang = app.config['LANGUAGES']
-    if localLang in supportLang:
-        return localLang
-    else:
-        return request.accept_languages.best_match(app.config['LANGUAGES'])
+def get_locale() -> str:
+    """
+    Gets locale from request object
+    """
+    locale = request.args.get('locale', '').strip()
+    if locale and locale in Config.LANGUAGES:
+        return locale
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
 
 
-if __name__ == "__main__":
+@app.route('/', strict_slashes=False)
+def index() -> str:
+    """
+    Renders a basic html template
+    """
+    return render_template('4-index.html')
+
+
+if __name__ == '__main__':
     app.run()
