ae8d07f443c18226c304f51ebb4090c301b82ce2
Rotondwa Ishmael Ndou <111059267+ndouishmael@users.noreply.github.com>
2023-08-01 20:36:48 +0200

    Update 4-app.py

--- 0x02-i18n/4-app.py
+++ 0x02-i18n/4-app.py
@@ -23,14 +23,12 @@ babel = Babel(app)
 def get_locale() -> str:
     """Retrieves the locale for a web page.
     """
-    queries = request.query_string.decode('utf-8').split('&')
-    query_table = dict(map(
-        lambda x: (x if '=' in x else '{}='.format(x)).split('='),
-        queries,
-    ))
-    if 'locale' in query_table:
-        if query_table['locale'] in app.config["LANGUAGES"]:
-            return query_table['locale']
+    locale = request.args.get('locale')
+
+    if locale and locale in app.config["LANGUAGES"]:
+        return locale
+
+    # Resort to the default behavior by using the best match based on accept languages
     return request.accept_languages.best_match(app.config["LANGUAGES"])
 
 
