2e59f3da96238944fc63b2f49cd5ac925834e1aa
Rotondwa Ishmael Ndou <111059267+ndouishmael@users.noreply.github.com>
2023-08-01 20:17:29 +0200

    Update 4-app.py

--- 0x02-i18n/4-app.py
+++ 0x02-i18n/4-app.py
@@ -1,8 +1,15 @@
 #!/usr/bin/env python3
 """ Basic Babel setup """
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, g
 from flask_babel import Babel, _
+from typing import Union
 
+users = {
+    1: {"name": "Balou", "locale": "fr", "timezone": "Europe/Paris"},
+    2: {"name": "Beyonce", "locale": "en", "timezone": "US/Central"},
+    3: {"name": "Spock", "locale": "kg", "timezone": "Vulcan"},
+    4: {"name": "Teletubby", "locale": None, "timezone": "Europe/London"},
+}
 
 class Config(object):
     """ Configuration Babel """
@@ -10,11 +17,29 @@ class Config(object):
     BABEL_DEFAULT_TIMEZONE = 'UTC'
     BABEL_DEFAULT_LOCALE = 'en'
 
-
 app = Flask(__name__, template_folder='templates')
 app.config.from_object(Config)
 babel = Babel(app)
 
+@app.before_request
+def before_request():
+    """ Request of each function """
+    g.user = get_user()
+
+def get_user() -> Union[dict, None]:
+    """ Get the user from the dictionary
+
+        Return User
+    """
+    login_user = request.args.get('login_as', None)
+
+    if login_user is None:
+        return None
+
+    user: dict = {}
+    user[login_user] = users.get(int(login_user))
+
+    return user[login_user]
 
 @babel.localeselector
 def get_locale():
@@ -28,8 +53,11 @@ def get_locale():
     if locale and locale in app.config['LANGUAGES']:
         return locale
 
-    return request.accept_languages.best_match(app.config['LANGUAGES'])
+    locale = request.headers.get('locale', None)
+    if locale and locale in app.config['LANGUAGES']:
+        return locale
 
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
 
 @app.route('/', methods=['GET'], strict_slashes=False)
 def hello_world():
@@ -38,8 +66,7 @@ def hello_world():
         Return:
             Initial template html
     """
-    return render_template('4-index.html')
-
+    return render_template('6-index.html')
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
