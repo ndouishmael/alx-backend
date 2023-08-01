186b0e52a1e3f9b0d563c44c8eaca7b57c317def
Rotondwa Ishmael Ndou <111059267+ndouishmael@users.noreply.github.com>
2023-08-01 21:11:22 +0200

    Update 4-app.py

--- 0x02-i18n/4-app.py
+++ 0x02-i18n/4-app.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 """0-app module
 """
-from flask import Flask, render_template, request, g
+from flask import Flask, render_template, request
 from flask_babel import Babel
 
 
